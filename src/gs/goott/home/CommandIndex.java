f3ad86edbfeaa05100d653ac0265606a9f2cb68b
zbass90 <smilejkh90@gmail.com>
2019-03-08 15:45:01 +0900

    1

--- src/gs/goott/home/CommandIndex.java
+++ src/gs/goott/home/CommandIndex.java
@@ -8,9 +8,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
-
-import gs.goott.content.ContentDAO;
 import gs.goott.controller.CommandService;
 import gs.goott.intro.IntroDAO;
 import gs.goott.intro.IntroVO;
