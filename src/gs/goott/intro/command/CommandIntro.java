1eb1258ae9fc31421f9bbaf372c0056ac392c916
hwi <khdrogba@gmail.com>
2019-03-13 11:12:30 +0900

    reply update

--- src/gs/goott/intro/command/CommandIntro.java
+++ src/gs/goott/intro/command/CommandIntro.java
@@ -1,6 +1,7 @@
 package gs.goott.intro.command;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -10,13 +11,15 @@ import javax.servlet.http.HttpSession;
 import gs.goott.controller.CommandService;
 import gs.goott.intro.IntroDAO;
 import gs.goott.intro.IntroVO;
+import gs.goott.intro.replyDAO;
+import gs.goott.intro.replyVO;
 import gs.goott.signup.MemberVO;
 
 public class CommandIntro implements CommandService {
 
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		System.out.println("intro gogo");
+		
 		HttpSession session = req.getSession();
 		int introNo = Integer.parseInt(req.getParameter("introNo"));
 		System.out.println("introNo="+introNo);
@@ -24,7 +27,11 @@ public class CommandIntro implements CommandService {
 		IntroDAO dao =new IntroDAO();
 		vo = dao.getIntro(introNo);
 		req.setAttribute("introVO", vo); //membervo 와 곂칠우려가 있어 introVO 로 이름을 지정		
-		System.out.println("VO="+vo.getUserid());
+	
+		replyDAO replyDao = new replyDAO();
+		List<replyVO> list = replyDao.getReply(introNo);	
+		req.setAttribute("list", list);
+		
 		return "intro.jsp";
 	}
 
